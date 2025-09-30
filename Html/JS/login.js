10e15a648ededdc9ba94fa4c90fdd92552a4d549
Matias Ouviña <you@example.com>
2025-09-30 14:24:20 -0300

    commit 2050

---
+++ Html/JS/login.js
@@ -0,0 +1,24 @@
+import fs from "fs"
+import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from "soquetic";
+const form = document.getElementById("login");
+const respuesta = document.getElementById("respuesta");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const data = { email, password };
+
+  postEvent("loginUsuario", data, (res) => {
+    respuesta.innerText = res.msg;
+    if (res.exito) {
+      console.log("Login exitoso");
+    } else {
+      console.log("Login fallido");
+    }
+  });
+
+  form.reset();
+});
