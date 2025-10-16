26248c994b10babc29c39549ae4dce152028ae21
Joaquin Sanchez Laffont <50159309@est.ort.edu.ar>
2025-10-16 08:45:30 -0300

    16 de oct

--- Html/JS/home.js
+++ Html/JS/home.js
@@ -1,2 +1,12 @@
-import fs from "fs"
-import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from "soquetic";
+const inputBuscar = document.getElementById("buscar");
+const botonBuscar = document.getElementById("boton-buscar");
+const resultado = document.getElementById("resultado");
+
+botonBuscar.addEventListener("click", function() {
+  const texto = inputBuscar.value;
+  if (texto === "") {
+    resultado.textContent = "no escribiste nada.";
+  } else {
+    resultado.textContent = "buscaste: " + texto;
+  }
+});
