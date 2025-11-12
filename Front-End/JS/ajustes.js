1ce19ea5b3c62a99ed6c5c01e62d03785ba30c9b
joaco <50159309@est.ort.edu.ar>
2025-11-12 11:32:52 -0300

    riccota commit

--- Front-End/JS/ajustes.js
+++ Front-End/JS/ajustes.js
@@ -1,39 +1,12 @@
-let btnClaro = document.getElementById("claro");
-let btnOscuro = document.getElementById("oscuro");
-let brillo = document.getElementById("brillo");
+connect2Server();
+
+const volumen = document.getElementById("volumen");
 
 window.addEventListener("DOMContentLoaded", () => {
-  let modo = localStorage.getItem("modo") || "claro";
-  aplicarModo(modo);
-
-  let brilloGuardado = localStorage.getItem("brillo");
-  if (brilloGuardado) {
-    brillo.value = brilloGuardado;
-    document.body.style.filter = `brightness(${brillo.value}%)`;
-  }
+  let volumenGuardado = localStorage.getItem("volumen") || 50;
+  volumen.value = volumenGuardado;
 });
 
-btnClaro.addEventListener("click", () => {
-  localStorage.setItem("modo", "claro");
-  aplicarModo("claro");
+volumen.addEventListener("input", () => {
+  localStorage.setItem("volumen", volumen.value);
 });
-
-btnOscuro.addEventListener("click", () => {
-  localStorage.setItem("modo", "oscuro");
-  aplicarModo("oscuro");
-});
-
-brillo.addEventListener("input", () => {
-  document.body.style.filter = `brightness(${brillo.value}%)`;
-  localStorage.setItem("brillo", brillo.value);
-});
-
-function aplicarModo(modo) {
-  if (modo === "oscuro") {
-    document.body.style.backgroundColor = "#1e1e1e";
-    document.body.style.color = "#ffffff";
-  } else {
-    document.body.style.backgroundColor = "#ffffff";
-    document.body.style.color = "#000000";
-  }
-}
